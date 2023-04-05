6a2643b3859adda1e4e1ca57b13189cfcbe567d8
Alex <a.caignard@hotmail.fr>
2023-04-05 15:58:17 +0200

    Créer un "Dark-light mode" (sans comm)

--- 8. Découverte de l'API de contexte de React/react-context/src/Components/BtnToggle/BtnToggle.js
+++ 8. Découverte de l'API de contexte de React/react-context/src/Components/BtnToggle/BtnToggle.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './BtnToggle.css';
+import { ThemeContext } from '../../Context/ThemeContext';
 
 export default function BtnToggle() {
+
+  const { toggleTheme, theme } = useContext(ThemeContext);
+
   return (
-    <button className='btn-toggle'>
-      Toggle
+    <button
+      onClick={toggleTheme}
+      className={theme ? " btn-toggle" : "btn-toggle dark-btn"}
+    >
+      {theme ? "LIGHT" : "DARK"}
     </button>
   )
 }
